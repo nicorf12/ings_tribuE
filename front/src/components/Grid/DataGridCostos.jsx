0f03ce6a27460806c0c0dcd97a43f8968db859a3
Sebastian <solaran@fi.uba.ar>
2024-11-24 19:36:26 -0300

    fix

--- front/src/components/Grid/DataGridCostos.jsx
+++ front/src/components/Grid/DataGridCostos.jsx
@@ -6,26 +6,28 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Container from "react-bootstrap/Container";
 
 // Función para crear datos
 function createData(name, ...values) {
     return { name, values };
 }
 
-
+const meses = ["Enero 2024","Febrero 2024"]
 
 // Componente DataGridCostos
 const DataGridCostos = (  ) => {
     // Crear filas de ejemplo (puedes modificar esto según tus datos)
     const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('Frozen yoghurt', 159, 6.0, 24,),
+        createData('Ice cream sandwich', 237, 9.0, 37, ),
+        createData('Eclair', 262, 16.0, 24, ),
+        createData('Cupcake', 305, 3.7, 67,),
+        createData('Gingerbread', 356, 16.0, 49, ),
     ];
 
     return (
+        <Container>
         <TableContainer component={Paper}  sx={{ backgroundColor: 'grey.300' }}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
@@ -49,12 +51,13 @@ const DataGridCostos = (  ) => {
                             {row.values.map((value, index) => (
                                 <TableCell key={index} align="right">{value}</TableCell>
                             ))}
-                            <TableCell align="right">{row.values.reduce((a, b) => a + b, 0)}</TableCell>
+
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
         </TableContainer>
+        </Container>
     );
 }
 
