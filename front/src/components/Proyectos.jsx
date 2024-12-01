24a8527867d7f541783072e9ed9c711adbb15e84
Sebastian <solaran@fi.uba.ar>
2024-12-01 17:43:27 -0300

    connect Proyectos.jsx con back

--- front/src/components/Proyectos.jsx
+++ front/src/components/Proyectos.jsx
@@ -1,33 +1,37 @@
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import DataGridProyectos from "./Grid/DataGridProyectos.jsx";
 import {useNavigate} from "react-router-dom";
+import {obtenerProyectos} from "../solicitudes.jsx";
 
 const Proyectos = () => {
     // Inicializa el estado de projects con los datos de los proyectos
-    const [projects, setProjects] = useState([
-        {
-            id: "a6e2167f-67a1-4f60-b9e9-6bae7bc3a15",
-            nombre: "Sistema de Gestión de Inventarios",
-            descripcion: "Un sistema para llevar el control de los productos en un almacén, incluyendo funciones para agregar, actualizar y eliminar productos, gestionar niveles de stock, y generar reportes de inventario. Suele incluir integración con otras áreas, como ventas y compras, y permite la optimización de recursos."
-        },
-        {
-            id: "1635b4ca-c091-472c-8b5a-cb3086d197b",
-            nombre: "Aplicación de Comercio Electrónico (E-commerce)",
-            descripcion: "Plataforma que permite a los usuarios comprar productos o servicios en línea. Incluye un catálogo de productos, un carrito de compras, métodos de pago seguros, y funciones de administración de pedidos y usuarios. A menudo, se desarrolla tanto la interfaz de usuario como el panel de administración."
-        },
-        {
-            id: "0d4e3470-4dc8-4fda-a08f-bb822cb9fc7f",
-            nombre: "Sistema de Gestión de Proyectos",
-            descripcion: "Herramienta para planificar, asignar, y hacer seguimiento a tareas dentro de proyectos. Incluye funcionalidades como la creación de proyectos, asignación de tareas a miembros del equipo, control de tiempos, gestión de recursos, y generación de reportes de progreso."
-        }
-    ]);
+    const [projects, setProjects] = useState([]);
 
     const navigate = useNavigate();
 
     const [project, setProject] = useState(null); // Para almacenar el proyecto seleccionado
+
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            let projects_aux
+            try {
+                projects_aux = await obtenerProyectos();
+
+            } catch (error) {
+                console.log("Error al obtener los proyectos:", error);
+                return;
+            }
+            setProjects(projects_aux);
+
+        };
+        fetchData();
+    }, [])
 
 
     const handleSubmit = (event) => {
@@ -35,9 +39,6 @@ const Proyectos = () => {
         console.log("Proyecto seleccionado:", project);
         navigate("/lider/proyecto", {state: project})
     };
-
-
-
 
 
     return (
